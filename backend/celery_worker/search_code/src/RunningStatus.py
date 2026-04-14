ef30321d1013445c4851d4c7dca97b2f0a797679
wu yuxuan <wuyuxuanmail@qq.com>
2026-04-14 23:25:21 +0800

    更新gitignore，清理已追踪的celery_worker和logs

--- backend/celery_worker/search_code/src/RunningStatus.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-# 1. 定义枚举
-class Status(Enum):
-    # 'Pending' | 'Running' | 'Success' | 'Failed'
-    PENDING = "Pending"
-    RUNNING = "Running"
-    SUCCESS = "Success"
-    FAILED = "Failed"
